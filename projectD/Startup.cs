163f83b7b6e90ed91b5b36eff374230ed4a068d6
lenkovets <a-lenkovez@agiks.ru>
2017-03-24 15:38:53 +0300

    add project

---
+++ projectD/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(projectD.Startup))]
+namespace projectD
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
